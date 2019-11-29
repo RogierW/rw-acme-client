9c5da6b0184c92ce2565fc0bc2c3e7a9f2545234
Rogier <rogier@isp24.nl>
2019-11-29 23:23:19 +0100

    add missing argument

--- src/Endpoints/Order.php
+++ src/Endpoints/Order.php
@@ -64,7 +64,7 @@ class Order extends Endpoint
     public function finalize(OrderData $orderData, string $csr): bool
     {
         if (!$orderData->isReady()) {
-            $this->client->logger("Order status for {$orderData->id} is $orderData->status. Cannot finalize order.");
+            $this->client->logger('error', "Order status for {$orderData->id} is $orderData->status. Cannot finalize order.");
 
             return false;
         }
